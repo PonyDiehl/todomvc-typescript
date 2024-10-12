80c9969542f95432dfbf8b7cf4b2fb3931be0d92
Cowboy Crypto <dwight@cowboycrypto.io>
2024-10-11 22:17:55 -0700

    Initial commit

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { generateId } from '../utils/generateId';
+import { Todo } from '../models/Todo';
+
+const TodoApp: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = () => {
+    if (inputValue.trim() === '') return;
+
+    const newTodo: Todo = {
+      id: generateId(),
+      text: inputValue,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setInputValue('');
+  };
+
+  const toggleTodo = (id: string) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new todo..."
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              onClick={() => toggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <button>check box</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
---
+++ src/models/Todo.ts
@@ -0,0 +1,7 @@
+// src/models/Todo.ts
+export interface Todo {
+    id: string;
+    text: string;
+    completed: boolean;
+  }
+  
